abb483d1c4a902272d6e0699641328141b356ed3
metehan <mthnzbk@gmail.com>
2016-08-04 21:17:13 +0300

    lisans bilgisi.

--- welcome/welcomeui.py
+++ welcome/welcomeui.py
@@ -1,3 +1,24 @@
+#
+#
+#  Copyright 2016 Metehan Özbek <mthnzbk@gmail.com>
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+#
+
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel, QSpacerItem, QSizePolicy, QCheckBox, QDesktopWidget
 from PyQt5.QtGui import QIcon, QDesktopServices, QPixmap
 from PyQt5.QtCore import Qt, QSysInfo, QSize, QUrl, QProcess
@@ -179,7 +200,7 @@ class WelcomeUi(QWidget):
         font.setPointSize(12)
         font.setBold(True)
         self.noteLabel.setFont(font)
-        self.noteLabel.setText(self.tr("Note: The username and password is \"live\"."))
+        self.noteLabel.setText(self.tr("Note: The Password is \"live\"."))
         self.noteLabel.setAlignment(Qt.AlignHCenter)
         self.contentWidget.layout().addWidget(self.noteLabel)
 
